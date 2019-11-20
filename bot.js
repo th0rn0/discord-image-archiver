c6d8a0840a81b1b3eb481598f836b72bccd898dc
Th0rn0 <Th0rn0@lanops.co.uk>
2019-11-20 21:56:31 +0000

    added youtube download and stats

--- bot.js
+++ bot.js
@@ -3,8 +3,8 @@ const client = new Discord.Client();
 const auth = require('./auth.json');
 const fs = require('fs');
 const download = require('download');
-
-var capitalize = require('capitalize')
+const ytdl = require('ytdl-core');
+var counter = {};
 
 client.on('ready', () => {
 	console.log('I am ready!');
@@ -21,14 +21,13 @@ client.on('message', message => {
 			return;
 		}
 		message.reply('Archiving the Channel...');
-		let archive_last_id = 646444707773415443;
     	getMessages(message.channel);
 	}
 });
 
 client.login(auth.token);
 
-async function getMessages(channel, limit = 10000) {
+async function getMessages(channel, limit = 50000) {
     const messageCount = [];
     let lastId;
 	client.user.setActivity('Archiving ' + channel.name + '!');
@@ -37,29 +36,129 @@ async function getMessages(channel, limit = 10000) {
         if (lastId) {
             options.before = lastId;
         }
-
         const messages = await channel.fetchMessages(options);
 		messages.forEach((message) => {
-			// console.log(message.content);
 			if (message.attachments.size > 0) {
 				message.attachments.forEach((attachment) => {
 					var counterAttach = 0;
 					console.log(attachment.url);
 					download(attachment.url).then(data => {
-					    fs.writeFileSync('dump/' + message.guild.name + '-' + message.channel.name + '-'  + message.id + '-' + message.author.username + '-' + attachment.filename, data);
+						// Set Timestamp
+						date = getTimestamp(message);
+						addCounter(message.member.user.username);
+						// Set Dir
+						dir = 'dump/' + message.guild.name + '/' + message.channel.name;
+						// Check if Dir Exists
+						if (!fs.existsSync(dir)){
+						    fs.mkdirSync(dir, { recursive: true });
+						}
+					    fs.writeFileSync(dir + '/'  + date + '-' + message.id + '-' + message.member.user.username + '-' + attachment.filename, data);
 					});
 				});
+			}
+			if (message.content.includes('youtu') && isYoutubeUrl(message.content)) {
+				// Set Timestamp
+				date = getTimestamp(message);
+				// Set Dir
+				dir = 'dump/' + message.guild.name + '/' + message.channel.name;
+				// Check if Dir Exists
+				if (!fs.existsSync(dir)){
+				    fs.mkdirSync(dir, { recursive: true });
+				}
+				ytdl.getInfo(message.content).then(data => {
+					console.log(data.title);
+					ytdl(message.content)
+	  					.pipe(fs.createWriteStream(dir + '/'  + date + '-' + message.id + '-' + message.member.user.username + '-' + cleanTitle(data.title) + '.flv'))
+	  					;
+				}).catch(error => console.error(error));
 			}
 		});
         messageCount.push(...messages.array());
         lastId = messages.last().id;
-        // console.log(messages);
         if (messages.size != 100 || messageCount >= limit) {
-        	channel.send('Images Archived');
+        	channel.send('Images & Videos Archived');
+        	channel.send(formatCounter());
+        	resetCounter();
 			client.user.setActivity('Ready to Archive!');
             break;
         }
     }
 
     return messageCount;
+}
+
+function isYoutubeUrl(url) {
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if ( match && match[7].length == 11 ){
+        return match[7];
+    }else{
+        return false;
+    }
+}
+
+function getTimestamp(message) {
+	var newDate = new Date();
+	newDate.setTime(message.createdTimestamp);
+	return newDate.getDate() + '.' + (newDate.getMonth()+1) + '.' + newDate.getFullYear() + '-' + newDate.getHours() + '.' + newDate.getMinutes() + '.' + newDate.getSeconds();
+}
+
+function addCounter(user) {
+	if (user in counter) {
+		counter[user] = counter[user] + 1;
+	} else {
+		counter[user] = 1;
+	}
+}
+
+function resetCounter() {
+	counter = {};
+}
+
+function formatCounter() {
+	var string = "Some stats for ur boiz \n";
+	string += "```";
+	string += "Some stats for ur boiz \n";
+	string += "A total of " + counterPersonCount(counter) + " people uploaded! \n";
+	string += "A total of " + counterUploadCount(counter) + " images and videos uploaded! \n";
+	for (var key in counter) {
+	  	string += key + ": " + counter[key] + "\n";
+	}
+	string += "```";
+	return string;
+}
+
+function counterUploadCount(counter) {
+  	var count = 0;
+  	for(var i in counter) {
+    	if( counter.hasOwnProperty( i ) ) {
+      		count += parseFloat( counter[i] );
+    	}
+  	}
+  	return count;
+}
+
+function counterPersonCount(counter) {
+	var count = 0;
+	for (var i in counter) {
+	    if (counter.hasOwnProperty(i)) {
+	        count++;
+	    }
+	}
+	return count;
+}
+
+function cleanTitle(string) {
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return string.toString().toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
 }
